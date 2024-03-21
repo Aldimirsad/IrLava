71f128ab050fe40daa484df40b3967fe13eeed93
Aldi <74283943+Aldimirsad@users.noreply.github.com>
2024-03-21 23:53:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-dca90717-9e3c-44de-bd70-58c4407fd887/)
 
 
 echo "$curl_output" >> Update.tsx
